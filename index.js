6a91749e29675ee3a08ccadf947fd7eed68c83e8
ChesterCheung <91168142+ChesterCheung@users.noreply.github.com>
2022-01-14 12:51:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your Circle class here
+class Formatter {
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+  return string.replace(/[^A-Za-z0-9-'" "]+/g, "");
+  
+  }  
+  // 1.Break string up into serparate strings
+  // 2.capitalize first letter of each string
+  // 3. join strings back together
+  static titleize(string){
+  let words = string.split(" ");
+  let array = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from', 'the']
+  for (let i = 0; i < words.length; i++) {
+    if(!array.includes(words[i].toLowerCase())){
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+  }
+  return words.join(" ")
+}
+}
